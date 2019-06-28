374f34ce1ac9e70a6bfcdbe036ef2ddda9ee0eba
kirsenkoDanIT <a.kirs@ukr.net>
2019-06-28 14:46:17 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,5 +4,9 @@ class Hamburger {
     constructor(size, stuffing) {
         this._size = size
         this._stuffing = stuffing
+        this._toppings = []
     }
+    get size(){}
+    get stuffing(){}
+    get toppings(){}
 }
