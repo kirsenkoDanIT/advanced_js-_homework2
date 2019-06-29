5bcaf63fc112464b6c90fcbf7ba0ada93bfc688f
kirsenkoDanIT <a.kirs@ukr.net>
2019-06-29 08:36:07 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,8 +5,37 @@ class Hamburger {
         this._size = size
         this._stuffing = stuffing
         this._toppings = []
+        console.log(arguments);
+        this._price = this._size.price
     }
-    get size(){}
-    get stuffing(){}
-    get toppings(){}
+
+
+    static SIZE_SMALL = () => {
+        return {
+            price: 50,
+            calories: 20,
+            category: 'size',
+            name: 'SIZE_SMALL',
+        }
+    }
+
+
+
+
+
+    // get size() {
+    //     return this._size
+    // }
+    // get stuffing() {
+    //     return this._stuffing
+    // }
+    // get toppings() {
+    //     return this._toppings
+    // }
 }
+
+const hamburger = new Hamburger(Hamburger.SIZE_SMALL())
+console.log(hamburger._size);
+console.log(hamburger._price);
+// console.dir(Hamburger);
+// console.log(Hamburger.SIZE_SMALL);
